9be28663125545e372edc2b47b49741a88360044
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-12-14 03:45:25 +0100

    fixed some API urls so they work

--- js/api/constants.js
+++ js/api/constants.js
@@ -1,7 +1,11 @@
-export const apiUrl = "https://api.noroff.dev/api/v1";
-// export const apiPath = apiUrl.toString()
-export const profileURL = `${apiUrl}/auction/profiles`;
-export const usersListing = `${profileURL}`;
+export const API_HOST = "https://api.noroff.dev/api/v1";
+export const API_AUCTION = "/auction";
+export const API_MAIN_URL = `${API_HOST}${API_AUCTION}`;
+export const profileURL = `${API_MAIN_URL}/profiles`;
+export const listingsURL = "/listings";
+export const bidsURL = "/bids";
+export const sellerBidsURL = "?_seller=true&_bids=true";
+
 
 // LocalStorage Exports
 export const profile = localStorage.getItem("profile");
