6f29cce805b012a84161de200f4fc8cc60c97db5
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-12-14 03:49:31 +0100

    fixed get profile function, works better now

--- js/api/profile/profile.js
+++ js/api/profile/profile.js
@@ -1,35 +1,41 @@
-import { apiUrl } from "../constants"; //future usage of this will come
-import { profileURL } from "../constants";
-import { profile } from "../constants";
-import { authFetch } from "../authHeader";
+import { profileURL } from "../constants.js";
+import { authFetch } from "../authHeader.js";
+import { load } from "../../storage/index.js";
 
+const user = load("userProfile");
+const userName = user.name;
+
+console.log(user);
+
+const method = "GET";
+
+const URL = `${profileURL}/${userName}`;
 const profileWrapper = document.getElementById("userWrapper");
 
-const loggedInUser = "?_seller=true&_count=true&_listings=true";
-
 /**
- * This is the function that gets information for users profile.
+ * function to fetch the profile info from the API 
  */
-async function getProfile() {
-  const getUserProfileUrl = `${profileURL}/${profile}${loggedInUser}`;
-  const response = await authFetch(getUserProfileUrl);
-  const data = await response.json();
-  console.log(data);
-  profileWrapper.innerHTML = "";
-  profileWrapper.innerHTML += `<div class="px-4 py-5 text-center border-bottom border-top solid shadow p-3 mb-5" id="usersProfile">
-    <img class="d-block mx-auto mb-4" src="${data.avatar}" alt="" width="72" height="57">
-    <h1 class="display-5 fw-bold">${data.name}</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">This is your statistics</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <p>Credits: ${data.credits}</p>
-        <p>Winning bids: ${data.wins}</p>
-      </div>
-    </div>
-  </div>`;
-  if (!response.ok) {
-    throw new Error("HTTP error! status: ${response.status}");
-  }
-}
+export async function getProfile() {
+  try {
+    const response = await authFetch(URL, method);
+    const userInfo = await response.json();
+    
+    profileWrapper.innerHTML = "";
+    profileWrapper.innerHTML += `<div class="px-4 py-5 text-center border-bottom border-top solid shadow p-3 mb-5" id="usersProfile">
+  <img class="d-block mx-auto mb-4 rounded" src="${userInfo.avatar}" alt="" width="100" height="90">
+  <h1 class="display-5 fw-bold">${userInfo.name}</h1>
+  <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4">This is your statistics</p>
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <p class="mb-4">Credits: ${userInfo.credits} <span class=mx-2>|</span> Wining Bids: ${userInfo.wins.length} <span class=mx-2>|</span> Listings: ${userInfo._count.listings}</p>
 
+    </div>
+  </div>
+</div>`
+  } catch (error) {
+    console.log(error);
+  }
+
+
+}
 getProfile();
