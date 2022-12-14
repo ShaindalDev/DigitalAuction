6388c1a1215758bcd190c4b1106e2f8219247d9f
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-12-14 23:02:27 +0100

    fixing users listing on the profile page

--- js/api/profile/usersListing.js
+++ js/api/profile/usersListing.js
@@ -1,9 +1,12 @@
-import { usersListing } from "../constants.js";
-import { profile } from "../constants.js";
-import { timeFormatChanger } from "../../components/timeChange"; //will be implemented later
+import { profileURL } from "../constants.js";
+import { listingsURL } from "../constants.js";
+import { load } from "../../storage/index.js";
+import { authFetch } from "../authHeader.js";
+import { renderItems} from "../../components/renderListings.js";
 
-const token = localStorage.getItem("_token");
-const userName = localStorage.getItem("profile");
+const token = load("token");
+const user = load("userProfile");
+const userName = user.name;
 const listingWrapper = document.getElementsByClassName("usersAuctions");
 /**
  * This will get all the user's post that has been created.
@@ -12,43 +15,52 @@ const listingWrapper = document.getElementsByClassName("usersAuctions");
  * @param {string} token This is the token that has been stored when login function was run
  */
 export async function getUserListing() {
-  const response = await fetch(`${usersListing}/shaindal/listings`, {
-    method: "get",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(),
-  });
-  const data = await response.json();
-  // console.log(data);
-  /**
-   * Filter function, to filter all the data that comes from the API, down to a single users data.
-   */
-  const filteredData = data.filter((filtered) => {
-    return filtered.name == userName;
-  });
+  const url = `${profileURL}/${userName}${listingsURL}?_seller=true&_bids=true`;
 
-  listingWrapper.innerHTML = "";
-  filteredData.forEach((filteredData) => {
-    listingWrapper.innerHTML += `<div class="col">
-    <div class="card py-3 px-3">
-      <img src="${filteredData.media}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${filteredData.title}</h5>
-        <p class="card-text">${filteredData.description}</p>
-        <p class="card-text">${filteredData.tags}</p>
-        <p class="card-text">${filteredData.bids}</p>
-      </div>
-    </div>
-  </div>`;
-  });
-  if (!response.ok) {
-    throw new Error("HTTP error! status: ${response.status}");
+  try {
+    const response = await authFetch(url);
+    const data = await response.json();
+
+    if (response.status === 200) {
+      renderItems(data);
+    }
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-  console.log("This is all of your posts", filteredData);
+  // const response = await fetch(`${profileURL}/${userName}${listingsURL}?_seller=true&_bids=true`, {
+  //   method: "get",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  //   body: JSON.stringify(),
+  // });
+  // const data = await response.json();
 
-  console.log(listingWrapper);
+  // console.log(data);
+
+  // listingWrapper.innerHTML = "";
+  //   listingWrapper.innerHTML += `<div class="col">
+  //   <div class="card py-3 px-3">
+  //     <img src="${data.media}" class="card-img-top" alt="...">
+  //     <div class="card-body">
+  //       <h5 class="card-title">${data.title}</h5>
+  //       <p class="card-text">${data.description}</p>
+  //       <p class="card-text">${data.tags}</p>
+  //       <p class="card-text">${data.bids}</p>
+        
+  //     </div>
+  //   </div>
+  // </div>`;
+
+  // if (!response.ok) {
+  //   throw new Error("HTTP error! status: ${response.status}");
+  // }
+  // console.log("This is all of your posts", data);
+
+  // console.log(listingWrapper);
 }
 
 getUserListing();
