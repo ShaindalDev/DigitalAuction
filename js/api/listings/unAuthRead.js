fa84a4edfc4a34eabb48cbe0106dc4fc2226bad1
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-12-16 19:39:09 +0100

    fixed search listings. It works now

--- js/api/listings/unAuthRead.js
+++ js/api/listings/unAuthRead.js
@@ -1,8 +1,9 @@
 import { API_MAIN_URL } from "../constants.js";
+import { searchPosts } from "../../components/searchFunction.js";
 import { renderItems } from "../../components/renderListings";
 
 export async function getUnAuthItems(getItemsURL) {
-  const url = `${API_MAIN_URL}/listings`;
+  const url = `${API_MAIN_URL}/listings?_active=true&_seller=true&_bids=true&sort=created&sortOrder=desc`;
   const method = "GET";
 
   try {
@@ -10,6 +11,8 @@ export async function getUnAuthItems(getItemsURL) {
     const items = await response.json(getItemsURL);
     console.log(items);
     renderItems(items);
+    searchPosts(items);
+
   } catch (error) {
     console.log(error);
   }
